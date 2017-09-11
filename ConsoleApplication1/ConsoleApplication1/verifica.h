c63f39ea0a21c51848287b60c81b2e2fc8b1a4d8
Jedielson <jed111_@hotmail.com>
2017-09-11 02:23:11 -0300

    Atualização biblioteca verifica

--- ConsoleApplication1/ConsoleApplication1/verifica.h
+++ ConsoleApplication1/ConsoleApplication1/verifica.h
@@ -28,6 +28,12 @@ int verificaresultado(char conferematriz[3][3]) {
 	if (conferematriz[0][2] == 'X' && conferematriz[1][1] == 'X' && conferematriz[2][0] == 'X') {
 		vencedor = 2;
 	}
+	if (conferematriz[0][1] == 'X' && conferematriz[1][1] == 'X' && conferematriz[2][1] == 'X') {
+		vencedor = 2;
+	}
+	if (conferematriz[0][2] == 'X' && conferematriz[1][2] == 'X' && conferematriz[2][2] == 'X') {
+		vencedor = 2;
+	}
 
 	//confere se o jogador 1 venceu
 	if (conferematriz[0][0] == 'O') {
@@ -48,6 +54,12 @@ int verificaresultado(char conferematriz[3][3]) {
 		vencedor = 1;
 	}
 	if (conferematriz[0][2] == 'O' && conferematriz[1][1] == 'O' && conferematriz[2][0] == 'O') {
+		vencedor = 1;
+	}
+	if (conferematriz[0][1] == 'O' && conferematriz[1][1] == 'O' && conferematriz[2][1] == 'O') {
+		vencedor = 1;
+	}
+	if (conferematriz[0][2] == 'O' && conferematriz[1][2] == 'O' && conferematriz[2][2] == 'O') {
 		vencedor = 1;
 	}
 	//verifica se toda a matriz foi preenchida
